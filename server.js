f31c8b162d80de7d4912aa045bf32f99b4b6535a
ZC <carter.zo3y@gmail.com>
2022-01-20 16:15:54 -0800

    cleanup from boilerplate (var -> const, package.json)

--- server.js
+++ server.js
@@ -1,28 +1,16 @@
-// server.js
-// where your node app starts
-
-// init project
 require('dotenv').config();
-var express = require('express');
-var app = express();
+const express = require('express');
+const cors = require('cors');
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+const app = express();
+const port = process.env.PORT || 3000;
 
-// http://expressjs.com/en/starter/static-files.html
+app.use(cors());
+
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
-});
-
-
-// your first API endpoint... 
-app.get('/api/hello', function (req, res) {
-  res.json({greeting: 'hello API'});
 });
 
 app.get('/api/whoami', function(req, res) {
@@ -36,7 +24,6 @@ app.get('/api/whoami', function(req, res) {
   });
 });
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+app.listen(port, function () {
+  console.log('Listening at: http://localhost:' + port);
 });
